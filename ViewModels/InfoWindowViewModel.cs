4c76a2b16e993414447d58bc23ecb9b7d29b8ded
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-04-17 11:37:51 +0300

    Changed main window according to new validation and label view

--- ViewModels/InfoWindowViewModel.cs
+++ ViewModels/InfoWindowViewModel.cs
@@ -1,31 +1,27 @@
 using System;
-using System.ComponentModel;
 using System.Windows;
 using Lab2.Models;
 using Lab2.Tools;
-using Lab2.Tools.Zodiac;
+using Lab2.Tools.Validation;
 
 namespace Lab2.ViewModels
 {
-    public class InfoWindowViewModel: INotifyPropertyChanged
+    public class InfoWindowViewModel
     {
         #region Private fields
-        private Person _person;
+
+        private readonly PersonValidation _validation = new PersonValidation();
+        private Person _person = new Person("","");
+        private RelayCommand<object> _showFullInfo;
+
         #endregion
-        #region Fields
 
-        public string Name { get; set; }
+        #region Public fields
 
-        // public DateTime Date
-        // {
-        //     get => _date;
-        //     set
-        //     {
-        //         SetDate(value);
-        //         PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Date"));
-        //     }
-        // }
-        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name  { get => _person.Name; set => _person.Name = value; }
+        public string Surname  { get => _person.Surname; set => _person.Surname = value; }
+        public string Email  { get => _person.Email; set => _person.Email = value; }
+
         #endregion
 
         #region Private methods
@@ -35,6 +31,28 @@ namespace Lab2.ViewModels
                 age.Year - 1, (age.Year - 1 > 1) ? "s" : "",
                 age.Month - 1, (age.Month - 1 > 1) ? "s" : "",
                 age.Day - 1, (age.Day - 1 > 1) ? "s" : "");
+        }
+        public RelayCommand<object> ShowFullInfo
+        {
+            get
+            {
+                return _showFullInfo ??= new RelayCommand<object>(_ => GoToFullInfo(), Validate);
+            }
+        }
+
+        private async void GoToFullInfo()
+        {
+            if (!Validate(_person))
+                MessageBox.Show("Incorrect input!");
+            else
+            {
+                MessageBox.Show($"Correct input for {_person.Name} {_person.Surname}");
+            }
+        }
+
+        private bool Validate(object obj)
+        {
+            return _validation.Validate(_person);
         }
         #endregion
     }
