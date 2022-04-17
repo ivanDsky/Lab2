7adb2aab3ef1d549d11fe419448a818b11ba2022
ZloyDI <dobrovolskyivan2002@gmail.com>
2022-04-17 12:32:55 +0300

    Added AgeToString Formatter

---
+++ Tools/Formatters/AgeToString.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Lab2.Tools.Formatters
+{
+    public class AgeToString
+    {
+        public static string Format(DateTime age)
+        {
+            return String.Format("{0} year{1}, {2} month{3}, {4} day{5}",
+                age.Year - 1, (age.Year - 1 > 1) ? "s" : "",
+                age.Month - 1, (age.Month - 1 > 1) ? "s" : "",
+                age.Day - 1, (age.Day - 1 > 1) ? "s" : "");
+        }
+    }
+}
